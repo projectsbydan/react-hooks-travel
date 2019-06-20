b4877b91124a44867530e6f3187d23da9c30a3c6
Dan Deaconu <dan.deaconu@outlook.com>
2019-06-20 11:26:22 +0200

    added webapp hello world

---
+++ WebApp/webpack.config.js
@@ -0,0 +1,26 @@
+const path = require("path");
+
+module.exports = {
+  entry: ["whatwg-fetch", "./app.tsx"],
+  output: {
+    filename: "code.js",
+    path: path.join(__dirname, "../WebService/WebService/wwwroot/"),
+  },
+
+  resolve: {
+    extensions: [".ts", ".tsx", ".json", ".js"],
+  },
+
+  module: {
+    rules: [
+      // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
+      { test: /\.tsx?$/, enforce: "pre", loader: "awesome-typescript-loader" },
+      // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+      { enforce: "pre", test: /\.js$/, loader: "source-map-loader" },
+    ],
+  },
+
+  plugins: [],
+
+  externals: {},
+};
