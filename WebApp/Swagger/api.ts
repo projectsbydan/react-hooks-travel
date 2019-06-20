9dca877a5f3aba4b6b32a4d31d2dee849c9ac5fe
Dan Deaconu <dan.deaconu@outlook.com>
2019-06-20 15:23:47 +0200

    front page done

--- WebApp/Swagger/api.ts
+++ WebApp/Swagger/api.ts
@@ -16,7 +16,7 @@ import * as url from "url";
 import * as portableFetch from "portable-fetch";
 import { Configuration } from "./configuration";
 
-const BASE_PATH = "/".replace(/\/+$/, "");
+const BASE_PATH = "https://localhost:5001".replace(/\/+$/, "");
 
 /**
  *
@@ -101,6 +101,12 @@ export interface Location {
    * @memberof Location
    */
   content?: string;
+  /**
+   *
+   * @type {string}
+   * @memberof Location
+   */
+  imageUrl?: string;
 }
 
 /**
--- WebApp/app.tsx
+++ WebApp/app.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { render } from "react-dom";
+import { Layout } from "./Components/layout/layout";
 
-const App = () => <div>hello world</div>;
+const App = () => <Layout />;
 
 render(<App />, document.querySelector("#root"));
