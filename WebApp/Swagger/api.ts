c26c0dec33fe8254f9f3cd7dc46afb58ba749416
Dan Deaconu <dan.deaconu@outlook.com>
2019-07-01 15:42:00 +0200

    updated base path for the webservice

--- WebApp/Swagger/api.ts
+++ WebApp/Swagger/api.ts
@@ -16,7 +16,7 @@ import * as url from "url";
 import * as portableFetch from "portable-fetch";
 import { Configuration } from "./configuration";
 
-const BASE_PATH = "https://localhost:5001".replace(/\/+$/, "");
+const BASE_PATH = "https://ltndantravel.azurewebsites.net/".replace(/\/+$/, "");
 
 /**
  *
